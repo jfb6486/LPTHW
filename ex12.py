8e4763f1d881597a29134679fe812ad73d8eb8be
Joel Bateman <batemanjoel4@gmail.com>
2019-08-22 12:15:26 -0700

    Create ex12.py
    Chapter 12

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# ex12.py
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print("So, you %r old, %r tall and %r heavy." % (
+	age, height, weight))
+
